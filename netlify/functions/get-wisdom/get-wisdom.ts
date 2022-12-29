b4e875443f80c12801d397d8d04cfef2e23ba5ad
Dan Walsh <danwalsh@Dans-MacBook-Pro.local>
2022-12-30 02:04:18 +1100

    done

--- netlify/functions/get-wisdom/get-wisdom.ts
+++ netlify/functions/get-wisdom/get-wisdom.ts
@@ -2,35 +2,12 @@ import type { Handler } from "@netlify/functions";
 import entries from "./entries.json";
 
 export const handler: Handler = async (event, context) => {
-  // const { name = "stranger" } = event.queryStringParameters;
-
   return {
     statusCode: 200,
     body: JSON.stringify(sample(entries)),
-    // body: JSON.stringify({
-    //   message: `Hello, ${name}!`,
-    // }),
   };
 };
 
 function sample(arr: Record<string, string>[]) {
   return arr[Math.floor(Math.random() * arr.length)];
 }
-
-// exports.handler = (event, context, callback) => {
-//   callback(null, {
-//     statusCode: 200,
-//     body:       JSON.stringify(sample(entries)),
-//   });
-// };
-
-// export const handler: Handler = async (event, context) => {
-//   const { name = "stranger" } = event.queryStringParameters;
-
-//   return {
-//     statusCode: 200,
-//     body: JSON.stringify({
-//       message: `Hello, ${name}!`,
-//     }),
-//   };
-// };
